267bc56c5f3abd0e049fb97777214cabc3003d58
riku <rikubon20040405@gmail.com>
2026-07-06 14:02:03 +0900

    Add unit test

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -4,4 +4,21 @@ public class Calculate {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public int getIndividualFee(int age) {
+    if (age <= 12) {
+      return 1000;
+    } else {
+      return 2000;
+    }
+  }
+
+  public int getTotalFee(int age, int groupSize) {
+    int totalFee = getIndividualFee(age) * groupSize;
+    if (groupSize >= 10) {
+      totalFee = (int) (totalFee * 0.9);
+    }
+    return totalFee;
+  }
+
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -11,4 +11,32 @@ public class CalculateTest {
     assertEquals(expected, calculate.sum(2, 3));
   }
 
+  @Test
+  public void testGetIndividualFeeUnder13() {
+    Calculate calculate = new Calculate();
+    int expected = 1000;
+    assertEquals(expected, calculate.getIndividualFee(10));
+  }
+
+  @Test
+  public void testGetIndividualFeeOver13() {
+    Calculate calculate = new Calculate();
+    int expected = 2000;
+    assertEquals(expected, calculate.getIndividualFee(25));
+  }
+
+  @Test
+  public void testGetTotalFeeNoDiscount() {
+    Calculate calculate = new Calculate();
+    int expected = 1000;
+    assertEquals(expected, calculate.getTotalFee(10, 1));
+  }
+
+  @Test
+  public void testGetTotalFeeDiscount() {
+    Calculate calculate = new Calculate();
+    int expected = 18000;
+    assertEquals(expected, calculate.getTotalFee(25, 10));
+  }
+
 }
